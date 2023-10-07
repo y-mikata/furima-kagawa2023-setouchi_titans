81377f3774a3d32b3dfb7b92c10242dc9425d0d4
yuri464946 <takasakiyuri555@gmail.com>
2023-10-07 10:21:01 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true 
-  validates :last_name_kana, presence: true
+
   validates :birthday, presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
+
+
