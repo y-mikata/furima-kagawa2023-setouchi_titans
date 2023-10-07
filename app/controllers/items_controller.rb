839498aa6c8cff0cf6385be2eaf91ed2d00d541b
shige59 <141386004+shige59@users.noreply.github.com>
2023-10-07 21:09:15 +0900

    ビューファイルにセレクトボックスを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,26 +7,26 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    @item.category_id = 1
-    @item.condition_id = 1
-    @item.postage_type_id = 1
-    @item.prefecture_id = 1
-    @item.shipping_time_id = 1
-    binding.pry
-
     if @item.valid?
       @item.save
       return redirect_to root_path
     else
-      render 'new', status: :unprocessable_entity
+      render "new", status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :price).merge(user_id: current_user.id)
-    # params.require(:item).permit(:name, :info, :category_id, :condition_id, :price, :postage_type_id, :prefecture_id, :shipping_time_id).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :name,
+      :info,
+      :category_id,
+      :condition_id,
+      :price,
+      :postage_type_id,
+      :prefecture_id,
+      :shipping_time_id
+    ).merge(user_id: current_user.id)
   end
 end
