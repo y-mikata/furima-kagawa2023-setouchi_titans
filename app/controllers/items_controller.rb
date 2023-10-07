1be51b5b2b1cfa6b2899b767f337c0d228737acd
shige59 <141386004+shige59@users.noreply.github.com>
2023-10-07 20:18:26 +0900

    エラー発生時にメッセージが表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :condition_id, :price, :postage_type_id, :prefecture_id, :shipping_time_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :price).merge(user_id: current_user.id)
+    # params.require(:item).permit(:name, :info, :category_id, :condition_id, :price, :postage_type_id, :prefecture_id, :shipping_time_id).merge(user_id: current_user.id)
   end
 end
