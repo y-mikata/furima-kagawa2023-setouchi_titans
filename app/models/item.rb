3643a312c1742e2c83a836e521d3bd721365fec2
shige59 <141386004+shige59@users.noreply.github.com>
2023-10-07 21:16:07 +0900

    itemsコントローラーとバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,11 @@ class Item < ApplicationRecord
   # has_one :order
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_type
+  belongs_to :prefecture
+  belongs_to :shipping_time
 
   validates :category_id, :condition_id, :postage_type_id, :prefecture_id,
    :shipping_time_id, numericality: { other_than: 1, message: "can't be blank" } 
