e3db3c2f755861b79cbf591fc46280d34b9d4c42
jmcquarr <4024761+jmcquarr@users.noreply.github.com>
2023-10-22 18:45:55 +0900

    スクリプトは適切のページでしか走らないようにした

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,13 +1,15 @@
-function calc (){
-  const itemPrice  = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const addTaxVal = Math.floor(itemPrice.value * 0.1);
-    const profitVal = Math.floor(itemPrice.value * 0.9);
-    const addTaxPrice  = document.getElementById("add-tax-price");
-    const profit  = document.getElementById("profit");
-    addTaxPrice.innerHTML = addTaxVal.toLocaleString();
-    profit.innerHTML = profitVal.toLocaleString();
-  });
-};
+function calc() {
+  if (document.getElementById("item-price") != null) {
+    const itemPrice = document.getElementById("item-price");
+    itemPrice.addEventListener("keyup", () => {
+      const addTaxVal = Math.floor(itemPrice.value * 0.1);
+      const profitVal = Math.floor(itemPrice.value * 0.9);
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const profit = document.getElementById("profit");
+      addTaxPrice.innerHTML = addTaxVal.toLocaleString();
+      profit.innerHTML = profitVal.toLocaleString();
+    });
+  }
+}
 
-window.addEventListener('turbo:load', calc);
+window.addEventListener("turbo:load", calc);
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,16 +1,23 @@
 const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey)
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+  const currentPath = window.location.pathname;
+  const orderPageFormat = /\/items\/\d+\/orders$/;
+  if (!orderPageFormat.test(currentPath)) return;
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-  const form = document.getElementById('charge-form')
+  const publicKey = gon.public_key;
+  const payjp = Payjp(publicKey);
+  const elements = payjp.elements();
+
+  const numberElement = elements.create("cardNumber");
+  const expiryElement = elements.create("cardExpiry");
+  const cvcElement = elements.create("cardCvc");
+
+  numberElement.mount("#number-form");
+  expiryElement.mount("#expiry-form");
+  cvcElement.mount("#cvc-form");
+
+  const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
+    e.preventDefault();
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
         console.log("responseerror");
@@ -26,7 +33,6 @@ const pay = () => {
       cvcElement.clear();
       document.getElementById("charge-form").submit();
     });
-    e.preventDefault();
   });
 };
 
