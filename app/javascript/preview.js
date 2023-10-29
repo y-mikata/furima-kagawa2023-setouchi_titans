28d8151d256eff7c2af4f72acb6bc1d281b3f602
y-mikata <yuko.mikata@gmail.com>
2023-10-29 21:23:27 +0900

    div要素とimg要素の生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,14 +3,16 @@ document.addEventListener('turbo:load', function(){
   const previewList = document.getElementById('previews');
   
   if (!itemForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="item[image]"');
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+    
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
   });
 });
