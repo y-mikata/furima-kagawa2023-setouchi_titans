21d61203dd3cd456d936ef1b97d14afd1564c59e
jmcquarr <4024761+jmcquarr@users.noreply.github.com>
2023-12-21 10:20:28 +0900

    マイページのルーティングを実装する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
   end
+  resources :users, only: :show
 end
