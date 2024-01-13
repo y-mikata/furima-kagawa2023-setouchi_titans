9a507e0d0938cd559fc8190f85b890b97f7d379a
jmcquarr <4024761+jmcquarr@users.noreply.github.com>
2024-01-13 18:13:10 +0900

    ユーザー制限を実装する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :likes, :items, :orders, :cards]
+  before_action :authenticate_user!
+  before_action :contributor_confirmation
   layout 'user'
   def show
   end
@@ -20,5 +22,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @user
   end
 end
